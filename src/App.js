8793eddc2d868461324583724ea7145024717662
facu <facuchaves@gmailcom>
2022-05-02 14:06:17 -0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./App.css";
-import { Link, Route } from "wouter";
-import { Home } from "./pages/Home";
-import { GifInfoPage } from "./pages/GifInfoPage";
-import { ListOfGifsPage } from "./pages/ListOfGifsPage";
+import { Route } from "wouter";
 import { PlayersPage } from "./pages/playersPage";
 import StaticContex from "./context/StaticContex";
-import { GifsContextProvider } from "./context/GifsContext";
 import { PlayersContextProvider } from "./context/PlayersContext";
 import { AppBar , Toolbar , IconButton , Typography, Button , BottomNavigation , BottomNavigationAction } from '@material-ui/core';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import { useTranslation } from "react-i18next";
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
@@ -25,8 +20,8 @@ function App() {
     //objeto de static context creado para usar las variables globales en toda la app
     <StaticContex.Provider
       value={{
-        name: "Lucas",
-        lastName: "Csulak",
+        name: "Facu",
+        lastName: "Chaves",
         isAdult: true,
       }}
     >
@@ -48,18 +43,6 @@ function App() {
           {/* objeto de static context creado para usar las variables globales en toda la app
               tener en cuenta que tiene que "envolver" los componentes donde voy a querer consumirlo
           */}
-          <GifsContextProvider>
-            <Route component={Home} path="/" />
-
-            <Route
-              //si vamos al componente ListOfGifsPage vemos que por los "params" recuperamos los valores de la url para "keyWordInput" y "rating"
-              //aca al path le agregue el ? porque si no enviamos el rating solo toma hasta el keyWordInput
-              component={ListOfGifsPage}
-              path="/gif/:keyWordInput/:rating?"
-            />
-
-            <Route component={GifInfoPage} path="/gif-info/:keyWordInput" />
-          </GifsContextProvider>
           
           <PlayersContextProvider>
             <Route
@@ -82,5 +65,4 @@ function App() {
   );
 }
 
-//export default withAuthenticator(App, true);
  export default App;
--- src/services/settings.js
+++
@@ -1,3 +0,0 @@
-export const API_KEY = 'Sc9MBZzmvQXVCYeC4imY4vOcOeQty4Hc'
-
-export const API_URL = 'https://api.giphy.com/v1'
