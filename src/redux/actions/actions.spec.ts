e464892bd36e5436089807d05dc77a748c2c5b15
Facundo.Lopez <facundo.lopez@climate.com>
2022-09-09 22:42:42 -0300

    Add new actions redux

--- src/redux/actions/actions.spec.ts
+++ src/redux/actions/actions.spec.ts
@@ -1,5 +1,56 @@
-import {searchEntities} from '.';
+import {
+  ACTIONS,
+  createEntity,
+  deleteEntity,
+  searchEntities,
+  searchEntity,
+  updateEntity,
+} from '.';
 
-test('searchEntities action', () => {
-  expect(searchEntities({q: 'query'})).toMatchSnapshot();
+test('shoudld search entities action', () => {
+  const query = {q: 'query'};
+  const action = searchEntities(query);
+
+  expect(action.type).toBe(ACTIONS.SEARCH_ENTITIES);
+  expect(action.payload?.query).toBe(query);
+});
+
+test('shoudld search entity action', () => {
+  const entityId = 1;
+  const action = searchEntity(entityId);
+
+  expect(action.type).toBe(ACTIONS.SEARCH_ENTITY);
+  expect(action.payload?.entityId).toBe(entityId);
+});
+
+test('shoudld create entity action', () => {
+  const entity = {
+    id: 1,
+    name: 'Entity name',
+    score: 10,
+  };
+  const action = createEntity(entity);
+
+  expect(action.type).toBe(ACTIONS.CREATE_ENTITY);
+  expect(action.payload?.entity).toBe(entity);
+});
+
+test('shoudld update entity action', () => {
+  const entity = {
+    id: 1,
+    name: 'Entity name',
+    score: 10,
+  };
+  const action = updateEntity(entity);
+
+  expect(action.type).toBe(ACTIONS.UPDATE_ENTITY);
+  expect(action.payload?.entity).toBe(entity);
+});
+
+test('shoudld delete entity action', () => {
+  const entityId = 1;
+  const action = deleteEntity(entityId);
+
+  expect(action.type).toBe(ACTIONS.DELETE_ENTITY);
+  expect(action.payload?.entityId).toBe(entityId);
 });
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,10 +1,42 @@
 export const ACTIONS = {
   SEARCH_ENTITIES: 'SEARCH_ENTITIES',
+  SEARCH_ENTITY: 'SEARCH_ENTITY',
+  CREATE_ENTITY: 'CREATE_ENTITY',
+  UPDATE_ENTITY: 'UPDATE_ENTITY',
+  DELETE_ENTITY: 'DELETE_ENTITY',
 };
 
 export const searchEntities = (query: any) => ({
   type: ACTIONS.SEARCH_ENTITIES,
   payload: {
     query,
+  },
+});
+
+export const searchEntity = (entityId: number) => ({
+  type: ACTIONS.SEARCH_ENTITY,
+  payload: {
+    entityId,
+  },
+});
+
+export const createEntity = (entity: any) => ({
+  type: ACTIONS.CREATE_ENTITY,
+  payload: {
+    entity,
+  },
+});
+
+export const updateEntity = (entity: any) => ({
+  type: ACTIONS.UPDATE_ENTITY,
+  payload: {
+    entity,
+  },
+});
+
+export const deleteEntity = (entityId: number) => ({
+  type: ACTIONS.DELETE_ENTITY,
+  payload: {
+    entityId,
   },
 });
